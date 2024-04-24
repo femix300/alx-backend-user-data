9fdbe818e282525d550a34c708b35ebfad386534
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-04-24 08:41:07 +0100

    added task two

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 '''Session Authentication Module'''
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     '''The session authentication class'''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''Creates a session id
+        Its uses this sesssion_id as the key and the user_id as the value
+        '''
+        if not user_id:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
