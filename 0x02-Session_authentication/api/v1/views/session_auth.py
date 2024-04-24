660e849cc0ca7c81eeac51f71c1272110f2b98ee
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-04-24 15:12:41 +0100

    fixed pycodestyle issues

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from api.v1.views import app_views
-import os
+from os import getenv
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -30,6 +30,6 @@ def auth_session_login():
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
-    response.set_cookie(os.getenv('SESSION_NAME', '_my_session_id'), session_id)
+    response.set_cookie(getenv('SESSION_NAME', '_my_session_id'), session_id)
 
     return response
